adfef632029727eb71d74d6d87498bab1935b22a
Zach Latta <zach@zachlatta.com>
2014-01-12 16:05:28 -0800

    Revert "Try to fix 'no app binary' GAE issue."
    This reverts commit 024b24623f9426f925f28544bf8a8d4c3553c460.

--- hackedu/hackedu.go
+++ hackedu/hackedu.go
@@ -3,8 +3,6 @@ package hackedu
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/realistschuckle/gohaml"
 )
 
 func init() {
@@ -12,10 +10,5 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	engine, err := gohaml.NewEngine("%h1 Hello, world!")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprint(w, engine.Render(nil))
+	fmt.Fprint(w, "Hello, world!")
 }
