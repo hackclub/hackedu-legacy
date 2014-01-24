6d1c82797b595933a274d7ae5aa16fd62c124c30
Zach Latta <zach@zachlatta.com>
2014-01-23 18:30:59 -0800

    Display a map on homepage.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('clientApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'google-maps'
 ])
   .config(function ($routeProvider) {
     $routeProvider
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,9 +2,28 @@
 
 angular.module('clientApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.map = {
+      center: {
+        latitude: 36,
+        longitude: -101
+      },
+      zoom: 4
+    };
+    
+    $scope.schools = [
+      {
+        'name': 'Austin High School',
+        'location': {
+          'latitude': '30.27382',
+          'longitude': '-97.76745'
+        }
+      },
+      {
+        'name': 'Thunderridge High School',
+        'location': {
+          'latitude': '39.5347968',
+          'longitude': '-105.01200670000003'
+        }
+      }
     ];
   });
