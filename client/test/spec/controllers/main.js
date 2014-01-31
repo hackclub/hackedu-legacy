9e609fd7657e903bf7c234c85230a61704b426c7
Zach Latta <zach@zachlatta.com>
2014-01-31 01:49:02 -0800

    Fixed tests for MainCtrl

--- client/test/spec/controllers/main.js
+++ client/test/spec/controllers/main.js
@@ -1,23 +1,23 @@
 'use strict';
 
 describe('Controller: MainCtrl', function () {
-
-  // load the controller's module
   beforeEach(module('clientApp'));
 
-  var MainCtrl,
-    scope;
+  var ctrl, scope;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    ctrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    //expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a map to the scope', function () {
+    expect(scope.map).toNotBe(null);
+  });
+
+  it('should attach schools to the scope', function () {
+    expect(scope.schools.length).toBe(2);
   });
 });
 
